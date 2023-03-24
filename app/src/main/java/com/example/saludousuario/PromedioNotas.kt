34f5ab66a554ccf2079cf8f2ab93ff04cddf18a6
Cristofert Ocampo <ocampocristofert@gmail.com>
2023-03-24 13:49:12 -0500

    Aplicacion completa

--- app/src/main/java/com/example/saludousuario/PromedioNotas.kt
+++ app/src/main/java/com/example/saludousuario/PromedioNotas.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class PromedioNotas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +20,40 @@ class PromedioNotas : AppCompatActivity() {
     private fun onClick(boton: Int) {
         when(boton){
             1->{
+                val campoNombre: EditText = findViewById(R.id.txtNombre)
+                val nombre = campoNombre.text
+                val campoMateria: EditText = findViewById(R.id.txtMateria)
+                val materia = campoMateria.text
+                val campoNota1: EditText = findViewById(R.id.txtDecimalNota1)
+                val nota1:Double = campoNota1.text.toString().toDouble()
+                val campoNota2: EditText = findViewById(R.id.txtDecimalNota2)
+                val nota2:Double = campoNota2.text.toString().toDouble()
+                val campoNota3: EditText = findViewById(R.id.txtDecimalNota3)
+                val nota3:Double = campoNota3.text.toString().toDouble()
+                var promedioNotas = (nota1 + nota2 + nota3) / 3;
+                var resultado= ""
+
+                if (promedioNotas >= 3.0){
+                    resultado = "Aprobo"
+                }else{
+                    resultado = "Desaprobo"
+                }
+
+               // Toast.makeText(this,"$nombre \n $materia \n $nota1 \n $nota2 \n $nota3 \n $promedioNotas", Toast.LENGTH_LONG).show()
+
                 val intent = Intent(this,ResultadoNotas::class.java)
+                var miBundle:Bundle = Bundle()
+
+                miBundle.putString("nombreEstudiante", "$nombre")
+                miBundle.putString("materiaEstudiante", "$materia")
+                miBundle.putString("nota1Estudiante", "$nota1")
+                miBundle.putString("nota2Estudiante", "$nota2 ")
+                miBundle.putString("nota3Estudiante", "$nota3")
+                miBundle.putString("promedioEstudiante", "$promedioNotas")
+                miBundle.putString("resultadoNota", "$resultado")
+
+                intent.putExtras(miBundle)
+
                 startActivity(intent)
             }
             2->{
--- app/src/main/java/com/example/saludousuario/ResultadoNotas.kt
+++ app/src/main/java/com/example/saludousuario/ResultadoNotas.kt
@@ -1,15 +1,38 @@
 package com.example.saludousuario
 
-import android.annotation.SuppressLint
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class ResultadoNotas : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado_notas)
+
+        val nombreEstudiante:TextView=findViewById(R.id.txtViewNombre)
+        val materiaEstudiante:TextView=findViewById(R.id.txtViewMateria)
+        val nota1Estudiante:TextView=findViewById(R.id.txtViewNota1)
+        val nota2Estudiante:TextView=findViewById(R.id.txtViewNota2)
+        val nota3Estudiante:TextView=findViewById(R.id.txtViewNota3)
+        val promedioEstudiante:TextView=findViewById(R.id.txtViewPromedio)
+        val resultadoNotas:TextView = findViewById(R.id.txtViewResultadoNotas)
+
+        val miBundle:Bundle?= this.intent.extras
+
+        if (miBundle != null){
+            nombreEstudiante.text = "${miBundle.getString("nombreEstudiante")}"
+            materiaEstudiante.text = "${miBundle.getString("materiaEstudiante")}"
+            nota1Estudiante.text = "${miBundle.getString("nota1Estudiante")}"
+            nota2Estudiante.text = "${miBundle.getString("nota2Estudiante")}"
+            nota3Estudiante.text = "${miBundle.getString("nota3Estudiante")}"
+            promedioEstudiante.text = "${miBundle.getString("promedioEstudiante")}"
+            resultadoNotas.text = "${miBundle.getString("resultadoNota")}"
+        }else{
+
+        }
 
         val btnCloneResultado:Button = findViewById(R.id.btnCloseResultado)
         btnCloneResultado.setOnClickListener { onClickResultado() }
